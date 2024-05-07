2c0ca8f1c1e30610743b4194b5cffd6500a14b6f
ratmir <ratmir.yuldashev28@mail.ru>
2024-05-08 02:29:45 +0300

    start new

--- src/main/java/org/example/Controllers/DocumentController.java
+++ src/main/java/org/example/Controllers/DocumentController.java
@@ -1,12 +1,16 @@
 package org.example.Controllers;
 
+import org.example.Models.Document;
 import org.example.dao.DocumentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/document")
@@ -28,5 +32,10 @@ public class DocumentController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("documents", documentDAO.show(id));
         return "document/show";
+    }
+
+    @GetMapping("/new")
+    public String newDocument(@ModelAttribute("documents") List<Document> documents) {
+        return "document/new";
     }
 }
